9bd8aff3166ee0372af2612b33c5d10d85b638de
The Funny Girl <97488579+The-Funny-Girl@users.noreply.github.com>
2023-09-14 13:52:00 -0600

    Update BazaarGuiScreen.java
    I want to die

--- src/main/java/com/bzhelper/mod/bz_helper/BazaarGuiScreen.java
+++ src/main/java/com/bzhelper/mod/bz_helper/BazaarGuiScreen.java
@@ -2,27 +2,32 @@ package com.bzhelper.mod.bz_helper;
 
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.until.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.client.util.math.MatrixStack;
 
-public class BazaarGuiScreen extends Screen {
-    protected BazaarGuiScreen() {
-        super(new LiteralText("Bazaar GUI"));
+public class BazaarGuiScreen extends
+Screen {
+    protected BazzarGuiScreen(Text title) 
+    {
+        super(title);
     }
 
     @Override
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float partialTicks) {
-        this.renderBackground(matrices);
+    public void render (MatrixStackmatrices, int mouseX, int mouseY, float partialTicks) {
+        this.renderBackground(matrieces);
         super.render(matrices, mouseX, mouseY, partialTicks);
     }
 
-    @Override
-    public void init() {
-        this.addButton(new ButtonWidget(this.width / 2 - 50, this.height / 2 - 10, 100, 20, new LiteralText("Close"), button -> this.onClose()));
-    }
+@Override
+protected void inti() 
+    {this.addButton(newButtonWidget(this.width / 2 - 50, this.height / 2 - 10, 100, 20 new LiteralText("Close"), 
+                                    button -> this.onClose()));
+        }
 
-    private void onClose() {
+    pritvate void onClose() {
         this.client.openScreen(null);
     }
 }
+        
+            
